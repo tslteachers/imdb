7ab891d848a9d9062f5110ff923637a8571001b7
Jeff Cohen <jeff@starterleague.com>
2013-07-24 09:59:30 -0500

    Enhance the seeds file and add a warm-up challenge

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Director.destroy_all
+
+ron = Director.new
+ron.name = "Ron Howard"
+ron.save
+
+steven = Director.new
+steven.name = "Steven Spielberg"
+steven.save
+
+george = Director.new
+george.name = "George Lucas"
+george.save
 
 list_of_movies = [
-  { title: 'Lincoln', year: 2012 },
-  { title: 'Apollo 13', year: 1995 },
-  { title: 'Star Wars', year: 1977 },
-  { title: 'Raiders of the Lost Ark', year: 1981 }
+  { title: 'Lincoln', year: 2012, director_id: steven.id },
+  { title: 'Apollo 13', year: 1995, director_id: ron.id },
+  { title: 'Star Wars', year: 1977, director_id: george.id },
+  { title: 'Raiders of the Lost Ark', year: 1981, director_id: steven.id }
 ]
 
 Movie.destroy_all
@@ -18,18 +25,8 @@ list_of_movies.each do |movie_data|
   m = Movie.new
   m.title = movie_data[:title]
   m.year = movie_data[:year]
+  m.director_id = movie_data[:director_id]
   m.save
 end
 
-
-
-
-
-
-
-
-# director_names = ["Ron Howard", "Steven Spielberg", "George Lucas"]
-# Ron Howard directed Apollo 13
-# George Lucas directed Star Wars
-# Steven Spielberg directed Lincoln and Raiders of the Lost Ark
-
+puts "There are now #{Movie.count} movies and #{Director.count} directors."
