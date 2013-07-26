33464b78006cc181bbbf14fcb7ef3dd828f01b85
Raghu Betina <rbetina@gmail.com>
2013-07-26 16:09:34 -0500

    Starting to write Movie#roles method

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,4 +4,9 @@ class Movie < ActiveRecord::Base
     Director.find_by_id(self.director_id)
   end
 
+  # Modify this to bring back the movie's roles
+  def roles
+    # Movie.where(:director_id => self.id)
+  end
+
 end
