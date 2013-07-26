ce0016b885da63a068ee1e90e712b4e546e8fa67
Raghu Betina <rbetina@gmail.com>
2013-07-26 16:05:44 -0500

    Completed role's association methods

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,2 +1,13 @@
 class Role < ActiveRecord::Base
+
+  def movie
+    Movie.find_by_id(self.movie_id)
+  end
+
+  def actor
+    Actor.find_by_id(self.actor_id)
+  end
+
+  # Add a similar method for looking up the role's actor
+  # Use the two methods to improve the roles show and index pages
 end
